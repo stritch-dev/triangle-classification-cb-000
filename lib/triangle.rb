fa14347708b3adb7aa7b2bd6c50b393ad4c27b90
stritch.dev <stritch.dev@gmail.com>
2017-08-11 21:11:15 -0500

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,63 +1,41 @@
-require 'pry'
-
 # identify triangles
 class Triangle
   attr_accessor :sides, :invalid
   def initialize(a, b, c)
-    invalid?(a, b ,c)      || 
-    scalene(a, b ,c)     || 
-    equilateral(a, b ,c) || 
-    isosceles(a, b ,c)
+    @a = a
+    @b = b
+    @c = c
+    scalene || equilateral || isosceles
   end
 
-  def invalid?(a, b, c)
-    invalid = false
-    puts "======>"
-    puts [a,b,c].any? {|x| x<= 0} 
-    puts "<====="
-    #reebinding.pry
-    if [a,b,c].any? {|x| x<= 0} 
-      invalid = true
-    else
-      if a+b <= c || a+c <= b || b+c <= a
-        invalid = true
-      end
-    end
-    if invalid
-      begin
-       raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    end
-    @invalid = invalid
+  def invalid?
+    [@a, @b, @c].any? { |x| x <= 0 } || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
   end
 
   def kind
-    puts 'entring kind +++++++++'
-    if invalid
+    if invalid?
       begin
-       raise TriangleError
+        raise TriangleError
       end
     end
     @kind
   end
 
-  def scalene(a, b, c)
-    [a, b, c].uniq!.nil? && @kind = :scalene
+  def scalene
+    [@a, @b, @c].uniq!.nil? && @kind = :scalene
   end
 
- def equilateral(a, b, c)
-   a == b && b == c && @kind = :equilateral
- end
+  def equilateral
+    @a == @b && @b == @c && @kind = :equilateral
+  end
 
- def isosceles(a, b, c)
-    (a == b || b == c || c == a) && @kind = :isosceles
- end
+  def isosceles
+    (@a == @b || @b == @c || @c == @a) && @kind = :isosceles
+  end
 end
 
 class TriangleError < StandardError
   def message
-   'not this'
+    'This is not a triangle'
   end
 end
